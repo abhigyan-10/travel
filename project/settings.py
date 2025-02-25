94e1d31ab8b9fadf52bfae41b949f594fcd33877
abhigyan <abhi251805@gmail.com>
2025-02-25 21:39:59 +0530

    second commit

--- project/settings.py
+++ project/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middeleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
